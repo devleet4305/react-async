2b654f968aedf9e154dc7b790d83a2d966bd9809
devleet4305 <tahajibmunna@gmail.com>
2026-05-18 16:48:50 +0600

    frasutating

--- src/components/Bottles.jsx
+++ src/components/Bottles.jsx
@@ -1,20 +1,11 @@
-import { use } from "react";
+import { use, useActionState } from "react";
 
-const Bottles = ({ bottlesPromise }) => {
-    const bottles = use(bottlesPromise);
+const Bottles = ({ bottles}) => {
+    const Data = use(bottles());
 
     return (
         <div>
-            <h1>Bottles: {bottles.length}</h1>
-            {
-                bottles.map(bottle => (
-                    <div key={bottle.id}>
-                        <h3>{bottle.name}</h3>
-                        <img src={bottle.img} alt={bottle.name} width="200" />
-                        <p>Price: ${bottle.price}</p>
-                    </div>
-                ))
-            }
+            
         </div>
     );
 };
